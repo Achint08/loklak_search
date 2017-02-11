919cc7a08d3c4a5994e771308f2cf1e7ec544e89
Achint08 <gr8achint.sharma@gmail.com>
2017-02-11 19:24:23 +0530

    Optional commit message22

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1066:function(n,o,c){n.exports=c(468)},468:function(n,o){}},[1066]);
+webpackJsonp([1,2],{1071:function(n,o,c){n.exports=c(468)},468:function(n,o){}},[1071]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
